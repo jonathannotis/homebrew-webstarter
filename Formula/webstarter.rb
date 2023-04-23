43db6ac4dca2d5232c665d7857be0d86a8cc35e7
Jonathan Notis <jrnotis@gmail.com>
2023-04-23 18:15:44 -0400

    url change again. Changed to normal .tar file

--- Formula/webstarter.rb
+++ Formula/webstarter.rb
@@ -3,8 +3,8 @@
 class Webstarter < Formula
   desc "Create a full stack project all in a single command"
   homepage "https://github.com/jonathannotis/web-starterkit-cli"
-  url "https://github.com/jonathannotis/web-starterkit-cli/releases/download/v1.0.1/webstarter.tgz"
-  sha256 "686af2ae71ee4419d9bbeb1362d2c83972c8cbae3dae66ebcbadd41f0768fb13"
+  url "https://github.com/jonathannotis/web-starterkit-cli/releases/download/v1.0.1/webstarter.tar"
+  sha256 "e08b65a40ffdc53aac423c8cc61ae626b357c320f9d937b8678f71459c17886e"
   license "MIT"
   version "1.0.1"
 
