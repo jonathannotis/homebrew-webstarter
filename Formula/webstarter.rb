096a30af11518e1a6fe077ef5fcfc1e884494f2a
Jonathan Notis <jrnotis@gmail.com>
2023-04-23 17:20:32 -0400

    forgot to add hash before

--- Formula/webstarter.rb
+++ Formula/webstarter.rb
@@ -4,7 +4,7 @@ class Webstarter < Formula
   desc "Create a full stack project all in a single command"
   homepage "https://github.com/jonathannotis/web-starterkit-cli"
   url "https://github.com/jonathannotis/web-starterkit-cli/releases/download/v1.0.1/webstarter.tar.gz"
-  sha256 "567587f5377d8cfdc7f637354b92e0ea229c60ca8637720a122b8b80a43c1a33"
+  sha256 "d2dce1a0997c29c4098862a092d05e2f3775a59175cd671716887e64fb77a039"
   license "MIT"
   version "1.0.1"
 
