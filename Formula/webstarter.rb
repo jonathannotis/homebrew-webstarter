c0942501f59d1de163d74aa35f6ecb4333cde7be
Jonathan Notis <jrnotis@gmail.com>
2023-04-27 19:27:11 -0400

    tar change

--- Formula/webstarter.rb
+++ Formula/webstarter.rb
@@ -4,7 +4,7 @@ class Webstarter < Formula
   desc "Create a full stack project all in a single command"
   homepage "https://github.com/jonathannotis/web-starterkit-cli"
   url "https://github.com/jonathannotis/web-starterkit-cli/releases/download/v1.0.1/webstarter.tar.bz2"
-  sha256 "c93cc8753e12aa3239eecf227854bdb547a24beea08d017b2e5193967c94283c"
+  sha256 "f209b9fcce20f651f19ff11b103f6297a9b4028faa386935999662f39cd3f4e1"
   license "MIT"
   version "1.0.1"
 
