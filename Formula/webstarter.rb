b7b654b6d1d85090a368526180fe9c11842369d3
Jonathan Notis <jrnotis@gmail.com>
2023-04-27 13:29:54 -0400

    figured out the problem

--- Formula/webstarter.rb
+++ Formula/webstarter.rb
@@ -4,7 +4,7 @@ class Webstarter < Formula
   desc "Create a full stack project all in a single command"
   homepage "https://github.com/jonathannotis/web-starterkit-cli"
   url "https://github.com/jonathannotis/web-starterkit-cli/releases/download/v1.0.1/webstarter.tar.bz2"
-  sha256 "fe1b151d8b22a7ef36d017d1e98475516e4afb021275271b1374b6efab084fc6"
+  sha256 "7e4642ca9abc2e1a8bd81bf31bee34879608ce877e8bc403ecbe4e50294b0f68"
   license "MIT"
   version "1.0.1"
 
