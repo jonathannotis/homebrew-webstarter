efdc8359bc1471f6efa2abc6866b893e386117e7
Jonathan Notis <jrnotis@gmail.com>
2023-04-27 17:14:35 -0400

    changed tar file and added assets

--- Formula/webstarter.rb
+++ Formula/webstarter.rb
@@ -4,12 +4,13 @@ class Webstarter < Formula
   desc "Create a full stack project all in a single command"
   homepage "https://github.com/jonathannotis/web-starterkit-cli"
   url "https://github.com/jonathannotis/web-starterkit-cli/releases/download/v1.0.1/webstarter.tar.bz2"
-  sha256 "7e4642ca9abc2e1a8bd81bf31bee34879608ce877e8bc403ecbe4e50294b0f68"
+  sha256 "47d455d899075610cd3e21a229182401ae407e17e6f1a4461cc82dd28b241dd9"
   license "MIT"
   version "1.0.1"
 
   def install
-    bin.install "webstarter"
     prefix.install "assets"
+
+    bin.install "webstarter"
   end
 end
