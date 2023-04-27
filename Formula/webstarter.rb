e33c75c5c6fd271d9ecccf9b7ae7c674498a6a5d
Jonathan Notis <jrnotis@gmail.com>
2023-04-27 10:43:42 -0400

    trying .tar.bz2

--- Formula/webstarter.rb
+++ Formula/webstarter.rb
@@ -3,8 +3,8 @@
 class Webstarter < Formula
   desc "Create a full stack project all in a single command"
   homepage "https://github.com/jonathannotis/web-starterkit-cli"
-  url "https://github.com/jonathannotis/web-starterkit-cli/releases/download/v1.0.1/webstarter.tar"
-  sha256 "e08b65a40ffdc53aac423c8cc61ae626b357c320f9d937b8678f71459c17886e"
+  url "https://github.com/jonathannotis/web-starterkit-cli/releases/download/v1.0.1/webstarter.tar.bz2"
+  sha256 "fe1b151d8b22a7ef36d017d1e98475516e4afb021275271b1374b6efab084fc6"
   license "MIT"
   version "1.0.1"
 
